62d2942c438ad6ad47a2419428863867d4158fb7
meadlai <meadlai@qq.com>
2020-12-02 10:38:35 +0800

    add warning message

--- scripts/change_this_as_master.sh
+++ scripts/change_this_as_master.sh
@@ -5,6 +5,38 @@
 source /var/lib/pgsql/scripts/ahead_config.sh
 
 
+echo '      *******************************************'
+echo '      *'
+echo '      * Warning:: This script will make this VM as a master node.'
+echo '      * Warning:: Please *UPDATE* slave VMs to synchronize to this master node.'
+echo '      * Warning:: This master_node IP is: '`hostname -I`
+echo '      * '`ip address`
+echo '      * Warning:: Users are able to execute DML and DLL in this database.'
+echo '      * Warning:: Keep READ-WRITE transactions in this VM.'
+echo '      *    '
+echo '      * Press Ctrl+C to quit.'
+echo '      *'
+echo '      *******************************************'
+var_input=''
+
+echo '      Input the word "master" to continue: '
+read -p '      Please Input: ' var_input
+
+##########################
+	if [ "$var_input" = "master" ]
+	then
+		echo "      Executing..."
+	else
+		echo "      Quit!"
+		exit 0
+	fi
+##########################
+
+echo '      The data folder is: '${PGDATA}
+echo '      The '
+
+
+
 cp ./config_master/postgresql.auto.conf ${PGDATA}/postgresql.auto.conf
 rm -f ${PGDATA}/standby.signal
 ## to_do: update other slave nodes
--- scripts/change_this_as_slave.sh
+++ scripts/change_this_as_slave.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
-#
-#
 
 source /var/lib/pgsql/scripts/ahead_config.sh
 
 
-echo 'The data folder is: '${PGDATA}
-echo 'The master node is: '${master_node}
+echo '      *******************************************'
+echo '      *'
+echo '      * Warning:: This script will make this VM as a slave node.'
+echo '      * Warning:: The data in this VM would be DELETED!'
+echo '      * Warning:: Pull the data from master_node: '${master_node}
+echo '      * Warning:: Users can NOT execute DML and DLL in this database.'
+echo '      * Warning:: Keep READ ONLY transactions in this VM.'
+echo '      *    '
+echo '      * Press Ctrl+C to quit.'
+echo '      *'
+echo '      *******************************************'
+var_input=''
+
+echo '      Input the word "slave" to continue: '
+read -p '      Please Input: ' var_input
+
+##########################
+	if [ "$var_input" = "slave" ]
+	then
+		echo "      Executing..."
+	else
+		echo "      Quit!"
+		exit 0
+	fi
+##########################
+
+echo '      The data folder is: '${PGDATA}
+echo '      The master node is: '${master_node}
 
 
 /usr/bin/expect <<-EOF
@@ -17,15 +41,13 @@ expect eof
 EOF
 
 
-/usr/bin/expect <<-EOF
-	spawn su root -c "systemctl stop postgresql-13"
-	expect "Password:"
-	send "${os_user_root_password}\n"
-expect eof
-EOF
+echo '      Try to remove all files in: '${PGDATA}
 
-rm -rf ${PGDATA}/
-echo 'The data folder was deleted: '${PGDATA}
+rm -rf ${PGDATA}
+
+echo '      execute result is: '$?
+
+echo '      The data folder was deleted: '${PGDATA}
 
 /usr/bin/expect <<-EOF
 	spawn pg_basebackup -h ${master_node} -U replicator -p 5432 -D ${PGDATA} -Fp -Xs -P -R
@@ -33,7 +55,7 @@ echo 'The data folder was deleted: '${PGDATA}
 	send "${db_user_replicator_password}\n"
 expect eof
 EOF
-echo 'Data pulled from master node: '${master_node}
+echo '      Data pulled from master node: '${master_node}
 
 cp ./config_slave/postgresql.auto.conf ${PGDATA}/postgresql.auto.conf
 sed -i 's/{maste_node}/'${master_node}'/' ${PGDATA}/postgresql.auto.conf
@@ -47,6 +69,9 @@ EOF
 
 
 
-echo 'Restarting postgresql-13'
+echo '      Restarting postgresql-13'
+echo '      Task completed!'
+echo '      '
+echo '      '
 
 
