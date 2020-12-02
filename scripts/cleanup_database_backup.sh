cb4d4c13402c16a9c1572f97013463a49c428afb
meadlai <meadlai@qq.com>
2020-12-02 20:55:53 +0800

    cleanup scripts

--- scripts/cleanup_database_backup.sh
+++ scripts/cleanup_database_backup.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+#
+#
+keep_days=1
+log_dir='/mnt/smb/'
+wal_nas_dir='/mnt/smb/'
+dir_postfix=`hostname`
+backup_full_database_dump_dir='/mnt/smb/full_database_dump_'$dir_postfix
+backup_full_database_snapshot_dir='/mnt/smb/full_database_snapshot_'$dir_postfix
+log_echo_prefix=' #backup_cleanup_script# '`date "+%F %T"`' #'${dir_postfix}'#'
+
+echo '      '
+echo '      *'
+echo '      * Cleanup the backup files'
+echo '      *'
+echo '      '
+find ${backup_full_database_dump_dir}/* -mtime +${keep_days} -exec rm {} \;
+echo '      '
+find ${backup_full_database_snapshot_dir}/* -mtime +${keep_days} -exec rm {} \;
+echo '      '
+echo '      Cleanup job done!'
+echo '      '
+echo '      '
--- scripts/cleanup_wal_file.sh
+++ scripts/cleanup_wal_file.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+#
+#
+keep_days=30
+log_dir='/mnt/smb/'
+wal_nas_dir='/mnt/smb/'
+dir_postfix=`hostname`
+wal_slave_archive_dir='/mnt/smb/wal_slave_archive_'$dir_postfix
+wal_master_live_dir='/mnt/smb/wal_master_live'
+log_echo_prefix=' #wal_cleanup_script# '`date "+%F %T"`' #'${dir_postfix}'#'
+
+echo '      '
+echo '      *'
+echo '      * Cleanup the WAL files'
+echo '      *'
+echo '      '
+find ${wal_master_live_dir}/* -mtime +${keep_days} -exec rm {} \;
+echo '      '
+find ${wal_slave_archive_dir}/* -mtime +${keep_days} -exec rm {} \;
+echo '      '
+echo '      Cleanup job done!'
+echo '      '
+echo '      '
